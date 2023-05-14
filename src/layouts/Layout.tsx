2ba326cb4201c5648d110bea3c4d424ad88a1596
mateuszraczka <mateusz.raczka@microsoft.wsei.edu.pl>
2023-05-14 21:04:12 +0200

    extend header

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,21 +1,15 @@
 interface LayoutProps {
-    header: React.ReactNode;
-    main: React.ReactNode;
-    footer: React.ReactNode;
+  header: React.ReactNode;
+  main: React.ReactNode;
+  footer: React.ReactNode;
 }
-const Layout = ({header, main, footer}: LayoutProps) =>{
-    return (
-        <div className="grid grid-rows-[1fr, 6fr, 1fr] h-screen">
-            <header>
-                {header}
-            </header>
-            <main>
-                {main}
-            </main>
-            <footer>
-                {footer}
-            </footer>
-        </div>
-    )
-}
+const Layout = ({ header, main, footer }: LayoutProps) => {
+  return (
+    <div className="grid grid-rows-[1fr_8fr_1fr] h-screen">
+      {header}
+      {main}
+      {footer}
+    </div>
+  );
+};
 export default Layout;
--- src/page/Header/index.tsx
+++ src/page/Header/index.tsx
@@ -1,8 +1,14 @@
+import Navigation from "./Navigation/index"
+import { AiOutlineBook } from "react-icons/ai";
 const Header = () => {
     return (
-        <>
-        <h1>Header</h1>
-        </>
+        <header className="flex items-center gap-20 select-none">
+            <div className="flex gap-1">
+                <AiOutlineBook className="text-3xl"/>
+                <h1>Exam Organizer</h1>
+            </div>
+            <Navigation></Navigation>
+        </header>
     )
 }
 export default Header;
