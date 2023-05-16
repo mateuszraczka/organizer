2f2dbb68856ff0998072ae387eb6a4941eb0d341
mateuszraczka <mateusz.raczka@microsoft.wsei.edu.pl>
2023-05-16 20:26:39 +0200

    added cards to exams page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Layout from "./layouts/Layout";
 import Header from "./page/Header/index";
-import Main from "./page/Main/index";
+import Main from "./page/Exams/index";
 import Footer from "./page/Footer/index";
 function App() {
   return <Layout header={<Header />} main={<Main />} footer={<Footer />} />;
--- src/page/Header/index.tsx
+++ src/page/Header/index.tsx
@@ -1,14 +1,36 @@
-import Navigation from "./Navigation/index"
-import { AiOutlineBook } from "react-icons/ai";
+import Navigation from "./Navigation/index";
+import {
+  MdOutlineLibraryBooks,
+  MdOutlineGroup,
+} from "react-icons/md";
+import Button from "../../components/Button";
+import Logo from "./Logo";
 const Header = () => {
-    return (
-        <header className="flex items-center gap-20 select-none">
-            <div className="flex gap-1">
-                <AiOutlineBook className="text-3xl"/>
-                <h1>Exam Organizer</h1>
-            </div>
-            <Navigation></Navigation>
-        </header>
-    )
-}
+  const handleButtonClick = (name: string) => {
+    switch (name) {
+      case "exams":
+        break;
+      case "classes":
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <header className="flex items-center gap-10 select-none shadow-md p-2">
+      <Logo></Logo>
+      <Navigation>
+        <Button variant="horizontal" name="exams">
+          <MdOutlineLibraryBooks />
+          Exams
+        </Button>
+        <Button variant="horizontal" name="classes">
+          <MdOutlineGroup />
+          Classes
+        </Button>
+      </Navigation>
+    </header>
+  );
+};
 export default Header;
