d8f50f7ac1146044b9ed7a13a1b956cc070bcec5
mateuszraczka <mateusz.raczka@microsoft.wsei.edu.pl>
2023-05-18 22:34:52 +0200

    created basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,52 @@ import Classes from "./page/Classes";
 import NewClass from "./page/NewClass";
 //END OF TEST IMPORTS
 import Footer from "./page/Footer/index";
+import { Routes, Route } from "react-router-dom";
+import ProtectionRoute from "./components/ProtectionRoute";
 function App() {
+  const user = "test";
   return (
-  <Layout header={<Header />} footer={<Footer />}>
-    <NewClass></NewClass>
-  </Layout>
+    <Layout header={<Header />} footer={<Footer />}>
+      <Routes>
+
+        <Route
+          path="/exams"
+          element={
+            <ProtectionRoute user={user}>
+              <Exams />
+            </ProtectionRoute>
+          }
+        />
+        <Route
+          path="/exams/edit"
+          element={
+            <ProtectionRoute user={user}>
+              <NewExam />
+            </ProtectionRoute>
+          }
+        />
+        <Route
+          path="/classes"
+          element={
+            <ProtectionRoute user={user}>
+              <Classes />
+            </ProtectionRoute>
+          }
+        />
+        <Route
+          path="/classes/edit"
+          element={
+            <ProtectionRoute user={user}>
+              <NewClass />
+            </ProtectionRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={<></>}
+        />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/page/Header/index.tsx
+++ src/page/Header/index.tsx
@@ -1,23 +1,29 @@
 import Navigation from "./Navigation/index";
-import {
-  MdOutlineLibraryBooks,
-  MdOutlineGroup,
-} from "react-icons/md";
+import { MdOutlineLibraryBooks, MdOutlineGroup } from "react-icons/md";
 import Button from "../../components/Button";
 import Logo from "./Logo";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="flex items-center gap-10 select-none shadow-md p-2">
       <Logo></Logo>
       <Navigation>
-        <Button variant="horizontal" name="exams" className="flex gap-1 p-2">
-          <MdOutlineLibraryBooks />
-          <span>Sprawdziany</span>
-        </Button>
-        <Button variant="horizontal" name="classes" className="flex gap-1 p-2">
-          <MdOutlineGroup />
-          <span>Klasy</span>
-        </Button>
+        <Link to="/exams">
+          <Button variant="horizontal" name="exams" className="flex gap-1 p-2">
+            <MdOutlineLibraryBooks />
+            <span>Sprawdziany</span>
+          </Button>
+        </Link>
+        <Link to="/classes">
+          <Button
+            variant="horizontal"
+            name="classes"
+            className="flex gap-1 p-2"
+          >
+            <MdOutlineGroup />
+            <span>Klasy</span>
+          </Button>
+        </Link>
       </Navigation>
     </header>
   );
